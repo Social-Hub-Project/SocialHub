ea316552ad45d41e896bb3882d8320d7ce42bf91
Pawel Wypych <pawelwypych4@gmail.com>
2023-03-13 16:10:39 +0100

    initial commit

---
+++ src/main/java/com/application/socialhub/SocialHubApplication.java
@@ -0,0 +1,13 @@
+package com.application.socialhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialHubApplication.class, args);
+	}
+
+}
