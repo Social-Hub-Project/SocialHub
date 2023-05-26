161fa450a03b84ef7ab999b55c03190a6d970f77
peter20011 <petricho@wp.pl>
2023-05-26 21:16:28 +0200

    added returned info about search user isFollowed

--- src/main/java/com/application/socialhub/config/SocketBrokerConfig.java
+++ src/main/java/com/application/socialhub/config/SocketBrokerConfig.java
@@ -1,25 +1,25 @@
-package com.application.socialhub.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.*;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class SocketBrokerConfig implements  WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/app/user/queue/specific-user");
-        config.setApplicationDestinationPrefixes("/spring-security-mvc-socket");
-        config.setUserDestinationPrefix("/app/user");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/app/chatRoom").setAllowedOrigins("*").withSockJS();
-        registry.addEndpoint("/app/chatRoom").setAllowedOrigins("*");
-    }
-}
+//package com.application.socialhub.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.*;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class SocketBrokerConfig implements  WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker("/app/user/queue/specific-user");
+//        config.setApplicationDestinationPrefixes("/spring-security-mvc-socket");
+//        config.setUserDestinationPrefix("/app/user");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/app/chatRoom").setAllowedOrigins("*").withSockJS();
+//        registry.addEndpoint("/app/chatRoom").setAllowedOrigins("*");
+//    }
+//}
